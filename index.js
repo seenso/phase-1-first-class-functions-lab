cd94afb0e9d0bb8f745351e49965559ae4e9ddbb
seenso <>
2021-11-01 14:20:39 -0800

    passed test for returnFirstTwoDrivers() & returnLastTwoDrivers()

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+  return arr.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (arr) => {
+  return arr.slice(-2);
+};
+
+let selectingDrivers = () => {};
+
+let createFareMultiplier = () => {};
+
+const fareDoubler = () => {};
+
+const fareTripler = () => {};
+
+let selectDifferentDrivers = (arrayOfDrivers, func) => {};
